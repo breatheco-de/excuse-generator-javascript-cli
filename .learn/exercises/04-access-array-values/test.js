2cfc93511def34c1091d561d8dea9cd9d6ee3db7
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-06-08 03:32:26 +0000

    test separated into two, one checks for existence, second one checks for value

--- .learn/exercises/04-access-array-values/test.js
+++ .learn/exercises/04-access-array-values/test.js
@@ -3,10 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const rewire = require('rewire');
 
-test("You should have an array called developers with the indicated elements", ()=>{
+test("You should have an array called developers", ()=>{
     const file = rewire(path.resolve(__dirname, '../../../app.js'));
     const developers = file.__get__("developers")
     expect(developers).toBeTruthy()
+})
+
+test("Developers array should have the indicated elements", ()=>{
+    const file = rewire(path.resolve(__dirname, '../../../app.js'));
+    const developers = file.__get__("developers")
     expect(developers).toEqual(["Karla", "Alex", "Joe", "Thomas"])
 })
 
