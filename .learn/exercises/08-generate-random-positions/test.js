5511e794b7456923027116591aa0f09fa0b51973
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-06-08 05:18:52 +0000

    separated tests for better understading of the issue

--- .learn/exercises/08-generate-random-positions/test.js
+++ .learn/exercises/08-generate-random-positions/test.js
@@ -2,34 +2,60 @@ const fs = require('fs');
 const path = require('path');
 const rewire = require('rewire');
 
-test("The arrays who, what, and when should exist with the indicated elements", ()=>{
+test("The who array should exist", () => {
     const file = rewire(path.resolve(__dirname, '../../../app.js'));
-
     const who = file.__get__("who");
-    const what = file.__get__("what");
-    const when = file.__get__("when");
-
-    // Check that the arrays exist
     expect(who).toBeTruthy();
-    expect(what).toBeTruthy();
-    expect(when).toBeTruthy();
+});
 
-    // Check that the arrays contain the expected elements
+test("The who array should have the indicated elements", () => {
+    const file = rewire(path.resolve(__dirname, '../../../app.js'));
+    const who = file.__get__("who");
     expect(who).toEqual(['The dog','My granma','His turtle','My bird']);
+});
+
+test("The what array should exist", () => {
+    const file = rewire(path.resolve(__dirname, '../../../app.js'));
+    const what = file.__get__("what");
+    expect(what).toBeTruthy();
+});
+
+test("The what array should have the indicated elements", () => {
+    const file = rewire(path.resolve(__dirname, '../../../app.js'));
+    const what = file.__get__("what");
     expect(what).toEqual(['ate','pissed','crushed','broked']);
+});
+
+test("The when array should exist", () => {
+    const file = rewire(path.resolve(__dirname, '../../../app.js'));
+    const when = file.__get__("when");
+    expect(when).toBeTruthy();
+});
+
+test("The when array should have the indicated elements", () => {
+    const file = rewire(path.resolve(__dirname, '../../../app.js'));
+    const when = file.__get__("when");
     expect(when).toEqual(['before the class','right in time','when I finished','during my lunch','while I was praying']);
-})
+});
 
-test("You should be generating a random index for each array", ()=>{
+
+test("You should generate a random index for the who array", () => {
     const file = fs.readFileSync(path.resolve(__dirname, '../../../app.js'), 'utf8');
+    const regexWho = /let\s+(\w+)\s*=\s*.*?Math\s*\.random\s*\(\s*\)\s*\*\s*who\.length\s*\)\s*;?/gm;
+    const hasWhoIndex = regexWho.test(file.toString());
+    expect(hasWhoIndex).toBeTruthy();
+});
 
-    const regexWho = /let\s+(\w+)\s*=\s*Math\s*\.floor\s*\(\s*Math\s*\.random\s*\(\s*\)\s*\*\s*who\.length\s*\)\s*;?/gm;
-    const regexWhat = /let\s+(\w+)\s*=\s*Math\s*\.floor\s*\(\s*Math\s*\.random\s*\(\s*\)\s*\*\s*what\.length\s*\)\s*;?/gm;
-    const regexWhen = /let\s+(\w+)\s*=\s*Math\s*\.floor\s*\(\s*Math\s*\.random\s*\(\s*\)\s*\*\s*when\.length\s*\)\s*;?/gm;
+test("You should generate a random index for the what array", () => {
+    const file = fs.readFileSync(path.resolve(__dirname, '../../../app.js'), 'utf8');
+    const regexWhat = /let\s+(\w+)\s*=\s*.*?Math\s*\.random\s*\(\s*\)\s*\*\s*what\.length\s*\)\s*;?/gm;
+    const hasWhatIndex = regexWhat.test(file.toString());
+    expect(hasWhatIndex).toBeTruthy();
+});
 
-    let hasWhoIndex = regexWho.test(file.toString());
-    let hasWhatIndex = regexWhat.test(file.toString());
-    let hasWhenIndex = regexWhen.test(file.toString());
-
-    expect(hasWhoIndex && hasWhatIndex && hasWhenIndex).toBeTruthy();
+test("You should generate a random index for the when array", () => {
+    const file = fs.readFileSync(path.resolve(__dirname, '../../../app.js'), 'utf8');
+    const regexWhen = /let\s+(\w+)\s*=\s*.*?Math\s*\.random\s*\(\s*\)\s*\*\s*when\.length\s*\)\s*;?/gm;
+    const hasWhenIndex = regexWhen.test(file.toString());
+    expect(hasWhenIndex).toBeTruthy();
 });
